bdf84ee01435b988761379b1a06e06ed052c25e9
Kevin Bell <bellKevin@pm.me>
2023-03-30 20:08:46 -0600

    Update location.js

--- util/location.js
+++ util/location.js
@@ -1,5 +1,5 @@
 // The API key is mine, you need to get your own from Google
-const GOOGLE_API_KEY = 'AIzaSyBCc6S0w6_zfHR1DG-xM0QitPs3bqSUE2o';
+const GOOGLE_API_KEY = 'AIzaSyAIq5vDxe7DH1KsqO33w4bh7F83ck9myZ4';
 
 export function getMapPreview(lat, lng) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
